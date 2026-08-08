863ca3d4ec20bde48cfa80e8c0a8909ffbdafb11
CAVES <caves@example.com>
2026-08-08 01:14:53 -0400

    Added favorites.

--- server.js
+++ server.js
@@ -177,7 +177,13 @@ const init = async (extension) => {
         alwaysVisible: true
     });
     /**
-     * (Optional) Adding a (non-clickable) button to create a logical album and add photos to it.
+     * (Optional) Adding a button to create a logical album and add photos to it.
+     * TODO: this is half baked solution. On reindex or DB reset (like after a version upgrade) the album will be lost or photos of the reindexed gallery will be missing.
+     * Possible workarounds:
+     * 1. (medium) store the keyword in a separate table and readd it after indexing (override that function in the extension)
+     * 2. (easy) Read the current DB after indexing and make sure that the indexing output contains all album tags.
+     * 3. (medium) Save the tag as a sidecar file next to the photo, so indexing will pick it up automatically.
+     *     * (hard) Alternative of this solution is to store the sidecar in the tmp folder and override the indexing to also read sidecars from the tmp folder.
      */
     extension.ui.addMediaButton({
         name: 'Add to album',
@@ -226,6 +232,36 @@ const init = async (extension) => {
         else {
             extension.Logger.warn('No album name provided');
         }
+    });
+    // Add a Favorites button (star outline) to toggle the pg-favorite tag
+    extension.ui.addMediaButton({
+        name: 'favorite',
+        svgIcon: {
+            viewBox: '0 0 576 512',
+            // Star outline (regular) icon; will be colored gold via CSS when active
+            items: '<path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17c-11.7-23.6-45.6-23.6-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.1 23 46 46.4 33.7L288 439.6l130.7 68.7c23.4 12.3 50.9-7.6 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4-124.3-65.4c-9.3-4.9-20.5-4.9-29.8 0L163.7 450.7l23.7-138.4c1.8-10.5-2.6-21.2-11.2-27.2L55.6 210.9l139.8-20.3c10.9-1.6 20.2-8.5 24.9-18.5L288 61.5l68.7 110.6c4.7 10 14 16.9 24.9 18.5l139.8 20.3-122.9 74.2c-8.6 6-13 16.7-11.2 27.2z"/>'
+        },
+        apiPath: 'toggle-favorite',
+        reloadContent: true,
+        // The button will always be visible; we will style it based on presence of the tag.
+        alwaysVisible: true,
+        // Optional: disable for non‑logged‑in users if desired
+        minUserRole: UserDTO_1.UserRoles.User
+    }, async (params, body, user, media, repository) => {
+        // Toggle the 'pg-favorite' keyword for this media
+        const favTag = 'pg-favorite';
+        media.metadata.keywords = media.metadata.keywords || [];
+        const idx = media.metadata.keywords.indexOf(favTag);
+        if (idx >= 0) {
+            // Remove favorite
+            media.metadata.keywords.splice(idx, 1);
+        }
+        else {
+            // Add favorite
+            media.metadata.keywords.push(favTag);
+        }
+        await repository.save(media);
+        // No need to return anything; UI will refresh because reloadContent is true
     });
     /**
      * (Optional) Creating a messenger. You can use it with TopPickJob to send photos
--- server.ts
+++ server.ts
@@ -255,6 +255,36 @@ export const init = async (extension: IExtensionObject<TestConfig>): Promise<voi
     }
   });
 
+  // Add a Favorites button (star outline) to toggle the pg-favorite tag
+  extension.ui.addMediaButton({
+    name: 'favorite',
+    svgIcon: {
+      viewBox: '0 0 576 512',
+      // Star outline (regular) icon; will be colored gold via CSS when active
+      items: '<path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17c-11.7-23.6-45.6-23.6-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.1 23 46 46.4 33.7L288 439.6l130.7 68.7c23.4 12.3 50.9-7.6 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4-124.3-65.4c-9.3-4.9-20.5-4.9-29.8 0L163.7 450.7l23.7-138.4c1.8-10.5-2.6-21.2-11.2-27.2L55.6 210.9l139.8-20.3c10.9-1.6 20.2-8.5 24.9-18.5L288 61.5l68.7 110.6c4.7 10 14 16.9 24.9 18.5l139.8 20.3-122.9 74.2c-8.6 6-13 16.7-11.2 27.2z"/>'
+    },
+    apiPath: 'toggle-favorite',
+    reloadContent: true,
+    // The button will always be visible; we will style it based on presence of the tag.
+    alwaysVisible: true,
+    // Optional: disable for non‑logged‑in users if desired
+    minUserRole: UserRoles.User
+  }, async (params, body, user, media, repository) => {
+    // Toggle the 'pg-favorite' keyword for this media
+    const favTag = 'pg-favorite';
+    media.metadata.keywords = media.metadata.keywords || [];
+    const idx = media.metadata.keywords.indexOf(favTag);
+    if (idx >= 0) {
+      // Remove favorite
+      media.metadata.keywords.splice(idx, 1);
+    } else {
+      // Add favorite
+      media.metadata.keywords.push(favTag);
+    }
+    await repository.save(media);
+    // No need to return anything; UI will refresh because reloadContent is true
+  });
+
   /**
    * (Optional) Creating a messenger. You can use it with TopPickJob to send photos
    */
